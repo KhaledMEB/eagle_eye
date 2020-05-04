a86f22440a42789557e679f30a0c29b4c11f7549
shaddow99 <gk_mebhah@esi.dz>
2020-05-04 15:58:17 +0100

    Initial commit

---
+++ eagle_eye/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for eagle_eye project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eagle_eye.settings')
+
+application = get_asgi_application()
