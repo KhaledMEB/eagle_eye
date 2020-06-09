0b9e234f4a6c03ac76e36b7738571374688a6023
shaddow99 <gk_mebhah@esi.dz>
2020-06-09 12:16:22 +0100

    Créer l'APP Accueil

--- eagle_eye/urls.py
+++ eagle_eye/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accueil.urls')),
     path('vehicules/', include('vehicules.urls')),
     path('calendrier/', include('calendrier.urls'))
 ]
