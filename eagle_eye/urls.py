d109b302d292ceeba5e3f525cf0c8e7699231ece
shaddow99 <gk_mebhah@esi.dz>
2020-06-09 11:58:27 +0100

    Ajouter URL Pattern pour l'App Calendrier

--- eagle_eye/urls.py
+++ eagle_eye/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vehicules/', include('vehicules.urls'))
+    path('vehicules/', include('vehicules.urls')),
+    path('calendrier/', include('calendrier.urls'))
 ]
