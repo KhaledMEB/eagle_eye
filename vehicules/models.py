e70a2a6963fd9de8313c1aa0f4204bdc14fa32bb
shaddow99 <gk_mebhah@esi.dz>
2020-06-14 11:07:45 +0100

    Create Vehicule model

--- vehicules/models.py
+++ vehicules/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Vehicule(models.Model):
+    nom = models.CharField(max_length=100)
+    vip = models.CharField(max_length=100)
+    matricule = models.CharField(max_length=20)
+    statut = models.CharField(max_length=20)
+    type_v = models.CharField(max_length=20)
+    groupe = models.CharField(max_length=20)
+    compteur = models.IntegerField()
+    conducteur = models.CharField(max_length=40)
